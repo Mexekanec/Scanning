258083c766d2afea8152d94c45daefce4607dde0
Ferrum <105937347+Mexekanec@users.noreply.github.com>
2022-07-05 12:48:46 +0300

    Update ClientRender.java

--- src/main/java/Examplemod/examplemod/client/ClientRender.java
+++ src/main/java/Examplemod/examplemod/client/ClientRender.java
@@ -9,19 +9,23 @@ public final class ClientRender {
     private boolean enableScan;
 
     @SubscribeEvent
-    public void onEntityRenderPre(final RenderLivingEvent.Pre<EntityPig> event) {
-        if (this.enableScan)
-            GL11.glDisable(GL11.GL_DEPTH_TEST);
+    public void onEntityRenderPre(final RenderLivingEvent.Pre event) {
+        if (this.enableScan) {
+            Entity entity=event.getEntity();
+            if (entity instanceof EntityPig)
+                GL11.glDisable(GL11.GL_DEPTH_TEST);
+        }
     }
 
     @SubscribeEvent
-    public void onEntityRenderPost(final RenderLivingEvent.Post<EntityPig> event) {
-        if (this.enableScan)
-            GL11.glEnable(GL11.GL_DEPTH_TEST);
+    public void onEntityRenderPost(final RenderLivingEvent.Post event) {
+        if (this.enableScan) {
+            Entity entity=event.getEntity();
+            if (entity instanceof EntityPig)
+                GL11.glEnable(GL11.GL_DEPTH_TEST);
+        }
     }
-
     public void setEnableScan(final boolean value) {
         this.enableScan = value;
     }
 }
-
